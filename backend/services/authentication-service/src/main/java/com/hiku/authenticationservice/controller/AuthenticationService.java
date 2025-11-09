024279796380c8d4f6756ea93dffab04a5df11a4
akcabra <andraz.klobucar@gmail.com>
2025-11-09 15:10:04 +0100

    HIKU-13: Added entrypoint for auth service and test API call

---
+++ backend/services/authentication-service/src/main/java/com/hiku/authenticationservice/controller/AuthenticationService.java
@@ -0,0 +1,8 @@
+package com.hiku.authenticationservice.controller;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/auth")
+public class AuthenticationService extends Application {
+}
